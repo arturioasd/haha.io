1586fd779768fa507f377358cda1c67e598f577c
arturioasd <132169879+arturioasd@users.noreply.github.com>
2025-01-14 00:09:43 +0500

    Add files via upload

--- burgermenu.js
+++ burgermenu.js
@@ -4,6 +4,7 @@
       let header__menu = document.querySelector('.header__menu');
       let filter = document.querySelector('.filter')
       let header__filter = document.querySelector('.header__filter')
+      let header__saved = document.querySelector('.header__saved')
       let body = document.querySelector('body');
       header__burger.addEventListener('click', (e)=> {
         header__burger.classList.toggle('active');
@@ -16,6 +17,7 @@
       header__filter.addEventListener('click', (e)=> {
         header__filter.classList.toggle('filter-replace');
         filter.classList.toggle('filter-swipe');
+        header__saved.classList.toggle('delete-element');
         header__burger.classList.toggle('delete-element');
         body.classList.toggle('lock');
       })
